a3823b521cc4a5468bf1cc001f195ecca4d85c57
tqer39 <tqer39@gmail.com>
2023-05-18 04:05:01 +0000

    🍱 github/blogと連携するblogプロジェクトの作成

---
+++ terraform/src/blog/terraform.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_version = "1.4.6"
+  required_providers {
+    # MEMO: hashicor/aws を上にすると hashicorp/vercel を参照してしまうため注意
+    aws = {
+      source  = "hashicorp/aws"
+      version = "4.66.1"
+    }
+    vercel = {
+      source  = "vercel/vercel"
+      version = "0.13.0"
+    }
+  }
+  backend "s3" {
+    bucket  = "terraform-tfstate-tqer39-072693953877-ap-northeast-1"
+    encrypt = true
+    key     = "terraform-vercel/terraform/src/blog.tfstate"
+    region  = "ap-northeast-1"
+  }
+}
